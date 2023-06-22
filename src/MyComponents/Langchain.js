7685c390fd9880a517cc17212e401a38c1412dbb
Shayaan Sultan <sultan.shayaan@gmail.com>
2023-06-22 12:25:31 +0500

    Testing .env hiding

---
+++ src/MyComponents/Langchain.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
+
+const chat = new ChatOpenAI({ temperature: 0 });
+
